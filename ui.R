0711e178881ed55fdcfc56a4c93c314f65eb38e2
szuwalski <c.s.szuwalski@gmail.com>
2015-06-24 15:05:05 -0700

    pop up messages for sliders

--- ui.R
+++ ui.R
@@ -36,12 +36,12 @@ shinyUI(fluidPage(navbarPage("",
                                       sidebarLayout(
                                         sidebarPanel(width=3,
                                           div(helpText("Management decisions (mouse over for description)"), style = "font-size:80%"),
-                                          tags$div(title="Proportion of adults harvested",sliderInput("A.harv","Adult harvest",min=0,max=1,value=defaultVals[1],step=.01), style = "font-size:60%"),
-                                          tags$div(title="Proportion of eggs harvested",sliderInput("E.harv","Egg harvest",min=0,max=1,value=defaultVals[2],step=.01), style = "font-size:60%"),
-                                          tags$div(title="Fraction of unfished biomass at which no harvest occurs",sliderInput("harvest.floor","Cutoff",min=0,max=1,value=defaultVals[3],step=.01), style = "font-size:60%"),
+                                          tags$div(title="Sets the fraction of the population harvest by commercial fisheries for adult herring",sliderInput("A.harv","Adult harvest",min=0,max=1,value=defaultVals[1],step=.01), style = "font-size:60%"),
+                                          tags$div(title="Sets the fraction of herring eggs that may be harvests as spawn-on-kep for commercial and traditional uses",sliderInput("E.harv","Egg harvest",min=0,max=1,value=defaultVals[2],step=.01), style = "font-size:60%"),
+                                          tags$div(title="Sets the population size at which commercial fisheries for herring are closed as a fraction of unfished biomass",sliderInput("harvest.floor","Cut-off",min=0,max=1,value=defaultVals[3],step=.01), style = "font-size:60%"),
                                           div(helpText("Environmental influence"),style="font-size:80%"),
-                                          tags$div(title="Higher values equal more variability",sliderInput("CV.recruit","Magnitude of variability",min=0,max=1,value=defaultVals[4],step=.01), style = "font-size:60%"),                                         
-                                          tags$div(title="Higher values equal more regime-like behavior",sliderInput("AR","Regime-like dynamics",min=0,max=1,value=defaultVals[5],step=.01), style = "font-size:60%" )                                         
+                                          tags$div(title="Sets how much herring recruitment fluctuates from year to year",sliderInput("CV.recruit","Recruitment variability",min=0,max=1,value=defaultVals[4],step=.01), style = "font-size:60%"),                                         
+                                          tags$div(title="Sets how similar ocean conditions are from year to year; larger values imply more regime-like behavior",sliderInput("AR","Ocean regimes",min=0,max=1,value=defaultVals[5],step=.01), style = "font-size:60%" )                                         
                                         ),
                                         mainPanel(plotOutput("biomass",width="100%",height=520))
                                       )
